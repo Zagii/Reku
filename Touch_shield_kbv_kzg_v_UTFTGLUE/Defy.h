306115f6f9a9fffa0b4bf852f38ae9e7c421ea89
kzg <krzysztof.zagawa@external.t-mobile.pl>
2017-12-12 16:40:49 +0100

    dodanie nowych ekranow

---
+++ Touch_shield_kbv_kzg_v_UTFTGLUE/Defy.h
@@ -0,0 +1,11 @@
+#ifndef Defy_h
+#define Defy_h
+
+#define JSON_PWM_NAWIEW 'N'  // pwm wiatrak 1
+#define JSON_PWM_WYWIEW 'W'   // pwm wiatrak 2
+#define JSON_ROZMRAZANIE_WIATRAKI 'R' //
+#define JSON_ROZMRAZANIE_GGWC 'G' //
+#define JSON_KOMINEK 'K' //
+#define JSON_AUTO 'A' //tryb automatycznego dobrania predkosci
+
+#endif
